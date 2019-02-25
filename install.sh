3f209080fd29ae86f61f7db35dca08f03034a3d4
listenerri <listenerri@gmail.com>
2019-02-25 22:33:24 +0800

    add: install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+VimDir=$(dirname $(realpath $0))
+echo "current vim directory is: $VimDir"
+
+if [[ -e $HOME/.vim  ]]; then
+    echo "backup exsit .vim to .vim-bak"
+    rm -rf $HOME/.vim-bak
+    mv -f $HOME/.vim $HOME/.vim-bak
+fi
+
+if [[ -e $HOME/.vimrc  ]]; then
+    echo "backup exsit .vimrc to .vimrc-bak"
+    mv -f $HOME/.vimrc $HOME/.vimrc-bak
+fi
+
+echo "create link from $VimDir to $HOME/.vim"
+ln -s -f $VimDir $HOME/.vim
+
+echo "install plugins..."
+vim -c PlugUpdate -c qa
+
+echo "install done!"
