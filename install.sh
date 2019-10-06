59244e7c37f7661b292423e23745b5a67581537e
listenerri <listenerri@gmail.com>
2019-10-07 00:25:52 +0800

    compatible osx

--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-VimDir=$(dirname $(readlink -e $0))
+IsOSX=$(uname -a | grep -i Darwin)
+
+if [[ $IsOSX ]]; then
+    if [[ -z $(which realpath) ]]; then
+        echo "require realpath command!"
+        exit -1
+    fi
+
+    if [[ -z $(shopt | grep autocd) ]]; then
+        echo "require install another version of bash using brew!"
+        exit -1
+    fi
+fi
+
+VimDir=$(dirname $(realpath $0))
 echo "current vim directory is: $VimDir"
 
 if [[ -e $HOME/.vim  ]]; then
